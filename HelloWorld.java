6fe370aef056cce2df88806ca721e7d4cb35b2ee
sriramjaii <161344944+sriramjaii@users.noreply.github.com>
2024-03-14 17:03:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
 public static void main(String[] args)
 {
 System.out.println("Hello World!")
-System.out.println("welcome to MYWORLD!")
+System.out.println("welcome to MYWORLD123!")
 }
 }
